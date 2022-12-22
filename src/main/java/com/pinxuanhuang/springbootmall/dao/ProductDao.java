fe7b8c2982bac13bdb288c8a651bcbab858172fa
PinxuanHuang <hatedog77777@gmail.com>
2022-12-22 22:49:06 +0800

    Add create product api

--- src/main/java/com/pinxuanhuang/springbootmall/dao/ProductDao.java
+++ src/main/java/com/pinxuanhuang/springbootmall/dao/ProductDao.java
@@ -1,7 +1,9 @@
 package com.pinxuanhuang.springbootmall.dao;
 
+import com.pinxuanhuang.springbootmall.dto.ProductRequest;
 import com.pinxuanhuang.springbootmall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/pinxuanhuang/springbootmall/service/ProductService.java
+++ src/main/java/com/pinxuanhuang/springbootmall/service/ProductService.java
@@ -1,7 +1,9 @@
 package com.pinxuanhuang.springbootmall.service;
 
+import com.pinxuanhuang.springbootmall.dto.ProductRequest;
 import com.pinxuanhuang.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 }
