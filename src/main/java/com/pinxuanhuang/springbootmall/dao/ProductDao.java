ae9869433bb6db51310388ccc57057e13406186a
PinxuanHuang <hatedog77777@gmail.com>
2022-12-27 19:26:32 +0800

    Add query all products api

--- src/main/java/com/pinxuanhuang/springbootmall/dao/ProductDao.java
+++ src/main/java/com/pinxuanhuang/springbootmall/dao/ProductDao.java
@@ -3,7 +3,10 @@ package com.pinxuanhuang.springbootmall.dao;
 import com.pinxuanhuang.springbootmall.dto.ProductRequest;
 import com.pinxuanhuang.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
--- src/main/java/com/pinxuanhuang/springbootmall/service/ProductService.java
+++ src/main/java/com/pinxuanhuang/springbootmall/service/ProductService.java
@@ -3,7 +3,10 @@ package com.pinxuanhuang.springbootmall.service;
 import com.pinxuanhuang.springbootmall.dto.ProductRequest;
 import com.pinxuanhuang.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
